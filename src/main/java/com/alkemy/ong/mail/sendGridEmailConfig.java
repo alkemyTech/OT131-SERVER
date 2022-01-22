4c6e0e96790064194bf02e6918c591008f64c3aa
Matias Castagno <Matiicastagno007@gmail.com>
2022-01-22 02:17:02 -0300

    cree service, controller, clase, config de email

---
+++ src/main/java/com/alkemy/ong/mail/sendGridEmailConfig.java
@@ -0,0 +1,22 @@
+package com.alkemy.ong.mail;
+
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.context.annotation.Configuration;
+
+import com.sendgrid.SendGrid;
+
+import org.springframework.beans.factory.annotation.Value;
+
+@Configuration
+public class sendGridEmailConfig {
+
+	
+	@Value("${sendgrid.key}")
+	private String key;
+	@Bean
+	public SendGrid getSendgris() {
+		
+		return new SendGrid(key);
+	}
+}
