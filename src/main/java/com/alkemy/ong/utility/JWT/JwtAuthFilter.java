3026fd80d3c109771b37c2161293d018b9858d5d
Alexis Dávila <alexis19031992@gmail.com>
2022-01-24 13:48:37 -0300

    jwt before merge users module

---
+++ src/main/java/com/alkemy/ong/utility/JWT/JwtAuthFilter.java
@@ -0,0 +1,75 @@
+package com.alkemy.ong.utility.JWT;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtAuthFilter {
+
+    private final String HEADER = "Authorization";
+    private final String PREFIX = "Bearer ";
+    private final String SECRET = "secret";
+    private final String SPACE = "";
+    private final String AUTHORITIES = "authorities";
+
+//    @Autowired
+//    private JWT jwt;
+
+    //@Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
+        try {
+            String authenticationHeader = request.getHeader("Authorization");
+            if (existJwtToken(authenticationHeader)) {
+                Claims claims = validateToken(request);
+                if (claims.get(AUTHORITIES) != null) {
+                    setUpAuth(claims);
+                } else {
+                    SecurityContextHolder.clearContext();
+                }
+            } else {
+                SecurityContextHolder.clearContext();
+            }
+            chain.doFilter(request, response);
+        } catch (ExpiredJwtException | 
+                UnsupportedJwtException | 
+                MalformedJwtException e) { //tomar en clase aparte para errores si es necesario, o utilizar los existentes
+            
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            ((HttpServletResponse) response).sendError(HttpServletResponse.SC_FORBIDDEN, e.getMessage());
+        }
+    }
+
+    private Claims validateToken(HttpServletRequest request) {
+        String jwtToken = request.getHeader(HEADER).replace(PREFIX, SPACE);
+        return Jwts.parser().setSigningKey(SECRET.getBytes()).parseClaimsJws(jwtToken).getBody();
+    }
+
+    private void setUpAuth(Claims claims) {
+        @SuppressWarnings("unchecked")
+        List<String> authorities = (List) claims.get(AUTHORITIES);
+
+        UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(claims.getSubject(), null,
+                authorities.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList()));
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+    }
+
+    private boolean existJwtToken(String authenticationHeader) {
+        return (authenticationHeader != null && authenticationHeader.startsWith(PREFIX));
+    }
+
+}
