d8e61b481401909b2063a5b2cbd6045da6861da4
Federico Fernandez Lafi <ffernandezlafi@gmail.com>
2022-01-24 11:54:07 -0300

    New Classes Roles & Users + Repos

---
+++ src/main/java/com/alkemy/ong/model/Roles.java
@@ -0,0 +1,52 @@
+package com.alkemy.ong.model;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+
+import com.alkemy.ong.util.RoleName;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.lang.Nullable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Entity
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners (AuditingEntityListener.class)
+public class Roles {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private RoleName name;
+    @Nullable
+    private String description;
+    @OneToMany
+    private List <Users> users;
+    
+    public Roles(@NotNull RoleName name, String description, List<Users> users) {
+        this.name = name;
+        this.description = description;
+        this.users = users;
+    }
+
+    
+}
