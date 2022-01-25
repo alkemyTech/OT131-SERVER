fb3ed0ba37e3a807efcaccd2df9321361d973cff
matiasluca72 <matias_soto72@hotmail.com>
2022-01-25 12:20:15 -0300

    Fix: improving code quality

--- src/main/java/com/alkemy/ong/util/Constants.java
+++ src/main/java/com/alkemy/ong/util/Constants.java
@@ -3,6 +3,4 @@ package com.alkemy.ong.util;
 public final class Constants {
 
     public static final String REQ_GET_MAPP_ACTIVITIES = "/activities";
-    public static final String[] URL_AUTH_REQUEST = {"/", "/auth/**", "/public/**", "/js/**", "/css/**"};
-
 }
