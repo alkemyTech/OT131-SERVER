f95d29ed9b1a5f69e78fcf291ab1ee02dbec8c21
matiasluca72 <matias_soto72@hotmail.com>
2022-01-26 10:40:48 -0300

    Fix: fixing conflicts

--- src/main/java/com/alkemy/ong/util/Constants.java
+++ src/main/java/com/alkemy/ong/util/Constants.java
@@ -2,14 +2,12 @@ package com.alkemy.ong.util;
 
 public final class Constants {
 
-<<<<<<< HEAD
     public static final String REQ_GET_MAPP_ACTIVITIES = "/activities";
-=======
 	public static final String REQ_GET_MAPP = "/organizations"; 
 	public static final String POINT_GET_MAPP = "/public/{name}";
 	public static final String ENTITY_NOT_FOUND = "ENTITY NOT FOUND";
 	public static final String NAME_EXIST = "THE NAME OF THE ORGANIZATION ALREADY EXISTS";
 	
   public Constants() {}
->>>>>>> develop
+
 }
