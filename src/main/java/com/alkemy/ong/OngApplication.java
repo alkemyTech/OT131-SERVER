c176f77263e6b330ef82c17a3ab3b6919dbff4b0
matiasluca72 <matias_soto72@hotmail.com>
2022-02-01 10:46:49 -0300

    restoring main method

---
+++ src/main/java/com/alkemy/ong/OngApplication.java
@@ -0,0 +1,29 @@
+package com.alkemy.ong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EntityScan(basePackages = {"com.alkemy.ong.model"})
+@ComponentScan(basePackages = {"com.*"})
+@EnableJpaRepositories(basePackages = {"com.alkemy.ong.repository"})
+@EnableJpaAuditing
+@EnableTransactionManagement
+@EnableWebMvc
+@RepositoryRestController
+@EnableAutoConfiguration
+@SpringBootApplication
+public class OngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OngApplication.class, args);
+	}
+
+}
