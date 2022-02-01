2be46d13bdc10dc7dc0e890210205269fc4740ff
Federico Fernandez Lafi <ffernandezlafi@gmail.com>
2022-02-01 18:50:42 -0300

    Feat: added swagger documentation endpoint Fix: on @builder annotation

--- src/main/java/com/alkemy/ong/controller/UsersController.java
+++ src/main/java/com/alkemy/ong/controller/UsersController.java
@@ -6,6 +6,7 @@ import com.alkemy.ong.dto.NewUsersDTO;
 import com.alkemy.ong.dto.LoginUsersDTO;
 import com.alkemy.ong.dto.UsersNoAuthDto;
 import com.alkemy.ong.dto.UsersOkDto;
+import com.alkemy.ong.model.Users;
 import com.alkemy.ong.service.UsersServiceImpl;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
+
 import static com.alkemy.ong.util.Constants.REQ_MAPP_CLASS_USER;
 import static com.alkemy.ong.util.Constants.REQ_MAPP_DELETE_LOGIN_USER;
 import static com.alkemy.ong.util.Constants.REQ_MAPP_POST_LOGIN_USER;
@@ -31,6 +39,15 @@ public class UsersController {
     @Autowired
     UsersServiceImpl usersService;
 
+    
+    @Operation(summary = "Login into the api")
+	 @ApiResponses(value = { 
+        @ApiResponse (responseCode = "200", description = "Login ok. Return credentials" , 
+   				    content = { @Content(mediaType = "application/json", 
+   				      schema = @Schema(implementation = Users.class)) }),
+   		  
+   		@ApiResponse(responseCode = "401", description = "Unathorized. Error in log credentials", 
+   		    content = @Content),})
     @PostMapping(value = REQ_MAPP_POST_LOGIN_USER)
     private ResponseEntity<?> userAuthLogin(@Valid @RequestBody LoginUsersDTO loginUser) throws Exception {
         try {
@@ -44,7 +61,16 @@ public class UsersController {
         }
 
     }
-
+    @Operation(summary = "Delete user from Database (set active as false)")
+	@ApiResponses(value = { 
+        @ApiResponse (responseCode = "200", description = "Login ok. Return credentials" , 
+   				    content = {@Content(mediaType = "application/json", 
+   				      schema = @Schema(implementation = Users.class)) }),
+   		  
+   		@ApiResponse(responseCode = "403", description = "Forbidden. Only admin users can delete registers from db", 
+   		    content = @Content),
+        @ApiResponse(responseCode = "404", description = "User not found. Wrong identifier", 
+   		    content = @Content)})
     @DeleteMapping(value = REQ_MAPP_DELETE_LOGIN_USER)
     private ResponseEntity<?> deleteUser(@PathVariable(name = "id") Long id) throws Exception {
         try {
@@ -56,6 +82,15 @@ public class UsersController {
         }
 
     }
+    @Operation(summary = "Register in to the api")
+	@ApiResponses(value = { 
+        @ApiResponse (responseCode = "200", description = "Register ok. Return credentials" , 
+   				    content = {@Content(mediaType = "application/json", 
+   				      schema = @Schema(implementation = Users.class)) }),
+   		  
+   		@ApiResponse(responseCode = "400", description = "Bad Request. Missing parameters on register, or username already exists", 
+   		    content = @Content),
+        })
 
     @PostMapping(value = REQ_MAPP_POST_REGISTER_USER)
     public ResponseEntity<?> register(@Valid @RequestBody NewUsersDTO userDTO) {       
--- src/main/java/com/alkemy/ong/model/Organizations.java
+++ src/main/java/com/alkemy/ong/model/Organizations.java
@@ -12,6 +12,7 @@ import javax.validation.constraints.NotNull;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,6 +21,7 @@ import lombok.NoArgsConstructor;
 @Data
 @Entity
 @NoArgsConstructor
+@AllArgsConstructor
 @Table(name="organizations")
 public class Organizations {
 	
