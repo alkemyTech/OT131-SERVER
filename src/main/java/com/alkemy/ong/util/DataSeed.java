642b3d930b3f9fa2914059462cd4c9284a079e30
Federico Fernandez Lafi <ffernandezlafi@gmail.com>
2022-02-01 17:56:27 -0300

    Feat: Added users and organizations dataseed

--- src/main/java/com/alkemy/ong/util/DataSeed.java
+++ src/main/java/com/alkemy/ong/util/DataSeed.java
@@ -68,12 +68,13 @@ public class DataSeed implements CommandLineRunner {
     private void loadOrganizationsData() {
 
         if (organizationsRepository.count() == 0){
-            Organizations organizations = Organizations.builder()
+            Organizations organization = Organizations.builder()
             .name("SomosMás")
             .aboutUsText("Desde 1997 en Somos Más trabajamos con los chicos y chicas, mamás y papás, abuelos y vecinos del barrio La Cava generando procesos de crecimiento y de inserción social. Uniendo las manos de todas las familias, las que viven en el barrio y las que viven fuera de él, es que podemos pensar, crear y garantizar estos procesos. Somos una asociación civil sin fines de lucro que se creó en 1997 con la intención de dar alimento a las familias del barrio. Con el tiempo fuimos involucrándonos con la comunidad y agrandando y mejorando nuestra capacidad de trabajo. Hoy somos un centro comunitario que acompaña a más de 700 personas a través de las áreas de: educación, deportes, primera infancia, salud, alimentación y trabajo social")
             .phone(1160112988)
             .email("somosfundacionmas@gmail.com")
             .build();
+
         }
 
     }
