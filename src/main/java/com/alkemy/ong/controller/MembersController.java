a79d78d5098056f9f3c16cd254a8a1a55f993dbe
Carolina Gomez <carolinagomez.gc@gmail.com>
2022-02-06 14:14:35 -0300

    add security to endpoint

--- src/main/java/com/alkemy/ong/controller/MembersController.java
+++ src/main/java/com/alkemy/ong/controller/MembersController.java
@@ -9,20 +9,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import static com.alkemy.ong.util.Constants.*;
-
 import java.util.List;
 import static com.alkemy.ong.util.Constants.GET_MAPP_LIST_MEMBERS;
 import com.alkemy.ong.dto.MemberDTO;
 import com.alkemy.ong.service.MembersService;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.media.Schema;
 
 
 
+@Tag(name = "Members", description = "Create, update and show Members")
 @RestController
 @RequestMapping(REQ_MAPP_MEMBERS)
 public class MembersController {
@@ -46,7 +45,9 @@ public class MembersController {
             @ApiResponse(responseCode = "400", description = "Invalid id supplied",
                     content = @Content),
             @ApiResponse(responseCode = "404", description = "Member not found",
-                    content = @Content) })
+                    content = @Content),
+            @ApiResponse(responseCode = "403", description = "Access denied",
+            content = @Content)})
     
     @DeleteMapping(REQ_MAPP_ID)
     public ResponseEntity<?> deleteMember(@PathVariable Long id) {
