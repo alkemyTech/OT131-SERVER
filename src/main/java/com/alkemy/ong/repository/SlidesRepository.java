464ab16d47ad3332ef28bdeb785885efd292ae67
matiasluca72 <matias_soto72@hotmail.com>
2022-02-11 11:58:20 -0300

    Fix: Infinite loop in petition response when creating a new Slide in POST

--- src/main/java/com/alkemy/ong/repository/SlidesRepository.java
+++ src/main/java/com/alkemy/ong/repository/SlidesRepository.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface SlidesRepository extends JpaRepository<Slides, Long> {
    
-    @Query("SELECT c FROM Slide c WHERE organization_id = :idOrg")
+    @Query("SELECT c FROM Slides c WHERE organization_id = :idOrg")
     public Optional <List<Slides>> findByOrganizationId (@Param ("idOrg") Long id);
 	
 	public List<Slides> findByOrderByOrderAsc();
