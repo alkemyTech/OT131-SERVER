667c1cf14de3f27157623a03edcc3c24fde7db75
Rodrii <rodriikc@gmail.com>
2022-02-21 12:51:54 -0300

    Paso textos a constantes

--- src/test/java/com/alkemy/ong/controller/NewsControllerTest.java
+++ src/test/java/com/alkemy/ong/controller/NewsControllerTest.java
@@ -3,6 +3,7 @@ package com.alkemy.ong.controller;
 import com.alkemy.ong.dto.NewsDTO;
 import com.alkemy.ong.exception.ParamNotFoundException;
 import com.alkemy.ong.service.NewsService;
+import static com.alkemy.ong.util.Constants.*;
 import java.io.IOException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
@@ -40,24 +41,24 @@ public class NewsControllerTest {
     @BeforeEach
     public void setUp() {
         this.newsDTO = NewsDTO.builder()
-                .name("name")
-                .content("content")
-                .image("image")
+                .name(FOO_STRING)
+                .content(FOO_STRING)
+                .image(FOO_STRING)
                 .idCategory(1L)
                 .build();
 
-        when(newsService.findById(2L)).thenThrow(new ParamNotFoundException(""));
-        when(newsService.update(2L, newsDTO)).thenThrow(new ParamNotFoundException(""));
-        doThrow(new ParamNotFoundException("")).when(newsService).deleteNew(2L);
-        when(newsService.getAll(2)).thenThrow(new ParamNotFoundException(""));
+        when(newsService.findById(2L)).thenThrow(new ParamNotFoundException(EMPTY_STRING));
+        when(newsService.update(2L, newsDTO)).thenThrow(new ParamNotFoundException(EMPTY_STRING));
+        doThrow(new ParamNotFoundException(EMPTY_STRING)).when(newsService).deleteNew(2L);
+        when(newsService.getAll(2)).thenThrow(new ParamNotFoundException(EMPTY_STRING));
     }
 
     @Test
-    @WithMockUser(roles = "ADMIN")
+    @WithMockUser(roles = ADMIN_ROLE)
     public void doNotSaveWithNullData() throws Exception {
 
         this.mockMvc.perform(MockMvcRequestBuilders
-                .post("/news")
+                .post(REQ_MAPP_NEWS)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(dtoToJson(new NewsDTO())))
                 .andExpect(status().isBadRequest());
@@ -66,11 +67,11 @@ public class NewsControllerTest {
     }
 
     @Test
-    @WithMockUser(roles = "ADMIN")
+    @WithMockUser(roles = ADMIN_ROLE)
     public void saveWithadminRole() throws Exception {
 
         this.mockMvc.perform(MockMvcRequestBuilders
-                .post("/news")
+                .post(REQ_MAPP_NEWS)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(dtoToJson(newsDTO)))
                 .andExpect(status().isCreated());
@@ -79,21 +80,21 @@ public class NewsControllerTest {
     }
 
     @Test
-    @WithMockUser(roles = "USER")
+    @WithMockUser(roles = USER_ROLE)
     public void doNotSaveWithUserRole() throws Exception {
 
         this.mockMvc.perform(MockMvcRequestBuilders
-                .post("/news")
+                .post(REQ_MAPP_NEWS)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(dtoToJson(newsDTO)))
                 .andExpect(status().isForbidden());
     }
 
     @Test
-    @WithMockUser(roles = "ADMIN") 
+    @WithMockUser(roles = ADMIN_ROLE)
     public void getByIdWhitAdminRole() throws Exception {
 
-        this.mockMvc.perform(get("/news/1")
+        this.mockMvc.perform(get(REQ_MAPP_NEWS + REQ_MAPP_ID, 1)
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
 
@@ -103,7 +104,7 @@ public class NewsControllerTest {
     @Test
     public void getByIdWhitoutRole() throws Exception {
 
-        this.mockMvc.perform(get("/news/1")
+        this.mockMvc.perform(get(REQ_MAPP_NEWS + REQ_MAPP_ID, 1)
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isForbidden());
 
@@ -111,10 +112,10 @@ public class NewsControllerTest {
     }
 
     @Test
-    @WithMockUser(roles = "ADMIN")
+    @WithMockUser(roles = ADMIN_ROLE)
     public void getByNonexistentId() throws Exception {
 
-        this.mockMvc.perform(get("/news/2")
+        this.mockMvc.perform(get(REQ_MAPP_NEWS + REQ_MAPP_ID, 2)
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest());
 
@@ -122,11 +123,11 @@ public class NewsControllerTest {
     }
 
     @Test
-    @WithMockUser(roles = "ADMIN")
+    @WithMockUser(roles = ADMIN_ROLE)
     public void updateWithAdminRole() throws Exception {
 
         this.mockMvc.perform(MockMvcRequestBuilders
-                .put("/news/1")
+                .put(REQ_MAPP_NEWS + REQ_MAPP_ID, 1)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(dtoToJson(newsDTO)))
                 .andExpect(status().isOk());
@@ -135,11 +136,11 @@ public class NewsControllerTest {
     }
 
     @Test
-    @WithMockUser(roles = "USER")
+    @WithMockUser(roles = USER_ROLE)
     public void doNotUpdateWithUserRole() throws Exception {
 
         this.mockMvc.perform(MockMvcRequestBuilders
-                .put("/news/1")
+                .put(REQ_MAPP_NEWS + REQ_MAPP_ID, 1)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(dtoToJson(newsDTO)))
                 .andExpect(status().isForbidden());
@@ -148,11 +149,11 @@ public class NewsControllerTest {
     }
 
     @Test
-    @WithMockUser(roles = "ADMIN")
+    @WithMockUser(roles = ADMIN_ROLE)
     public void doNotUpdateWithNonexistentId() throws Exception {
 
         this.mockMvc.perform(MockMvcRequestBuilders
-                .put("/news/2")
+                .put(REQ_MAPP_NEWS + REQ_MAPP_ID, 2)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(dtoToJson(newsDTO)))
                 .andExpect(status().isBadRequest());
@@ -161,11 +162,11 @@ public class NewsControllerTest {
     }
 
     @Test
-    @WithMockUser(roles = "ADMIN")
+    @WithMockUser(roles = ADMIN_ROLE)
     public void doNotUpdateWithNullData() throws Exception {
 
         this.mockMvc.perform(MockMvcRequestBuilders
-                .put("/news/1")
+                .put(REQ_MAPP_NEWS + REQ_MAPP_ID, 1)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(dtoToJson(new NewsDTO())))
                 .andExpect(status().isBadRequest());
@@ -174,55 +175,55 @@ public class NewsControllerTest {
     }
 
     @Test
-    @WithMockUser(roles = "ADMIN")
+    @WithMockUser(roles = ADMIN_ROLE)
     public void deleteWithAdminRole() throws Exception {
 
         this.mockMvc.perform(MockMvcRequestBuilders
-                .delete("/news/1"))
+                .delete(REQ_MAPP_NEWS + REQ_MAPP_ID, 1))
                 .andExpect(status().isOk());
 
         verify(newsService, times(1)).deleteNew(1L);
     }
 
     @Test
-    @WithMockUser(roles = "USER")
+    @WithMockUser(roles = USER_ROLE)
     public void deleteWithUserRole() throws Exception {
 
         this.mockMvc.perform(MockMvcRequestBuilders
-                .delete("/news/1"))
+                .delete(REQ_MAPP_NEWS + REQ_MAPP_ID, 1))
                 .andExpect(status().isForbidden());
 
         verify(newsService, times(0)).deleteNew(1L);
     }
 
     @Test
-    @WithMockUser(roles = "ADMIN")
+    @WithMockUser(roles = ADMIN_ROLE)
     public void deleteWithNonexistentId() throws Exception {
 
         this.mockMvc.perform(MockMvcRequestBuilders
-                .delete("/news/2"))
+                .delete(REQ_MAPP_NEWS + REQ_MAPP_ID, 2))
                 .andExpect(status().isBadRequest());
 
         verify(newsService, times(1)).deleteNew(2L);
     }
 
     @Test
-    @WithMockUser(roles = "ADMIN")
+    @WithMockUser(roles = ADMIN_ROLE)
     public void getAllWithAdminRole() throws Exception {
 
         this.mockMvc.perform(MockMvcRequestBuilders
-                .get("/news?page=1"))
+                .get(REQ_MAPP_NEWS + URL_PAGE_TEST, 1))
                 .andExpect(status().isOk());
 
         verify(newsService, times(1)).getAll(1);
     }
-    
+
     @Test
-    @WithMockUser(roles = "USER")
+    @WithMockUser(roles = USER_ROLE)
     public void getAllWithUserRole() throws Exception {
 
         this.mockMvc.perform(MockMvcRequestBuilders
-                .get("/news?page=1"))
+                .get(REQ_MAPP_NEWS + URL_PAGE_TEST, 1))
                 .andExpect(status().isOk());
 
         verify(newsService, times(1)).getAll(1);
@@ -232,18 +233,18 @@ public class NewsControllerTest {
     public void getAllWithoutRole() throws Exception {
 
         this.mockMvc.perform(MockMvcRequestBuilders
-                .get("/news?page=1"))
+                .get(REQ_MAPP_NEWS + URL_PAGE_TEST, 1))
                 .andExpect(status().isForbidden());
 
         verify(newsService, times(0)).getAll(1);
     }
 
     @Test
-    @WithMockUser(roles = "ADMIN")
+    @WithMockUser(roles = ADMIN_ROLE)
     public void getAllWithNonexistentId() throws Exception {
 
         this.mockMvc.perform(MockMvcRequestBuilders
-                .get("/news?page=2"))
+                .get(REQ_MAPP_NEWS + URL_PAGE_TEST, 2))
                 .andExpect(status().isBadRequest());
 
         verify(newsService, times(1)).getAll(2);
