3aaff94a782ad73034f32de12251785f65a6c360
matiasluca72 <matias_soto72@hotmail.com>
2022-02-22 20:23:26 -0300

    Probando Users Testing

--- src/main/java/com/alkemy/ong/dto/LoginUsersDTO.java
+++ src/main/java/com/alkemy/ong/dto/LoginUsersDTO.java
@@ -1,9 +1,12 @@
 package com.alkemy.ong.dto;
 
 import javax.validation.constraints.NotBlank;
+
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class LoginUsersDTO{
 
     @NotBlank(message = "Email can't be empty")
--- src/main/java/com/alkemy/ong/dto/NewUsersDTO.java
+++ src/main/java/com/alkemy/ong/dto/NewUsersDTO.java
@@ -3,9 +3,13 @@ package com.alkemy.ong.dto;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 public class NewUsersDTO {
 
     @NotBlank(message = "First name can't be null")
@@ -21,4 +25,14 @@ public class NewUsersDTO {
     @Size(min = 8, message = "The password must have at least 8 characters")
     @NotBlank(message = "Password can't be null")
     private String password;
+
+    public NewUsersDTO() {
+    }
+
+    public NewUsersDTO(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
 }
